9a69408ee3f07e0e87481f3636d9fe07b4ebead4
liusy <hiwings@126.com>
2016-10-30 16:45:41 +0800

    add angular demo

---
+++ app.js
@@ -0,0 +1,14 @@
+
+angular.module('app', ['kylinno.switchery'])
+
+    .controller('MyController', function($scope) {
+        $scope.enabled = true;
+        $scope.onOff = true;
+        $scope.yesNo = true;
+        $scope.disabled = true;
+
+
+        $scope.changeCallback = function() {
+            console.log('This is the state of my model ' + $scope.enabled);
+        };
+    });
